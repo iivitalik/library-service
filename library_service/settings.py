3b9378ff30d11895820e76973b604a6ff318499a
msi <lg.mobile0808@gmail.com>
2025-12-13 22:42:22 +0200

    removed secret key from settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@*d8j3rb&4qb!4!heg&*uo9&!if1m6&x$++x)g67wd@$-ix2**'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
